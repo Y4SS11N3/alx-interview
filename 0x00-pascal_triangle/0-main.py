87afa7da3bec6e274f7fe614d75c31a907d5834b
YASSINE <contact@yassine.fun>
2024-07-23 04:48:49 -0700

    Pascal's Triangle

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -6,7 +6,7 @@ pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
 
 def print_triangle(triangle):
     """
-    Print the triangle
+    Print the triangle.
     """
     for row in triangle:
         print("[{}]".format(",".join([str(x) for x in row])))
