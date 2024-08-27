3d01c9c76e72584cfb75f827be1b738ea285c7e5
YASSINE <contact@yassine.fun>
2024-08-27 14:30:23 -0700

    N Queens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+"""
+N Queens solver
+"""
+import sys
+
+
+def is_safe(board, row, col, n):
+    """
+    Check if it's safe to place a queen at board[row][col]
+    """
+    # Check this row on left side
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    # Check upper diagonal on left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # Check lower diagonal on left side
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solve_nqueens(n):
+    """
+    Solve the N Queens problem and yield solutions
+    """
+    board = [[0 for _ in range(n)] for _ in range(n)]
+
+    def solve_util(col):
+        if col >= n:
+            solution = []
+            for i in range(n):
+                for j in range(n):
+                    if board[i][j] == 1:
+                        solution.append([i, j])
+            yield solution
+            return
+
+        for i in range(n):
+            if is_safe(board, i, col, n):
+                board[i][col] = 1
+                yield from solve_util(col + 1)
+                board[i][col] = 0
+
+    yield from solve_util(0)
+
+
+def print_solutions(solutions):
+    """
+    Print the solutions in the required format
+    """
+    for solution in solutions:
+        print(solution)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solutions = list(solve_nqueens(n))
+    print_solutions(solutions)
