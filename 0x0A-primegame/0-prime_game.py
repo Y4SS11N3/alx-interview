ff46477ca559a785ee9a529c4011d19449c36c05
YASSINE <contact@yassine.fun>
2024-10-01 18:27:22 -0700

    Fix: Optimize prime game for all cases

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,46 +4,26 @@ Prime Game Solution
 """
 
 
-def is_prime(n):
-    """Check if a number is prime."""
-    if n < 2:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-
-def get_primes(n):
-    """Get all prime numbers up to n."""
-    primes = []
-    for i in range(2, n + 1):
-        if is_prime(i):
-            primes.append(i)
-    return primes
-
-
 def isWinner(x, nums):
     """
     Determine the winner of the Prime Game.
-
-    Args:
-    x (int): The number of rounds.
-    nums (list): An array of n for each round.
-
-    Returns:
-    str: Name of the player that won the most rounds.
-         If the winner cannot be determined, return None.
     """
     if not nums or x < 1:
         return None
 
-    maria_wins = 0
-    ben_wins = 0
+    max_num = max(nums)
+    primes = [True] * (max_num + 1)
+    primes[0] = primes[1] = False
 
+    for i in range(2, int(max_num**0.5) + 1):
+        if primes[i]:
+            for j in range(i*i, max_num + 1, i):
+                primes[j] = False
+
+    maria_wins = ben_wins = 0
     for n in nums:
-        primes = get_primes(n)
-        if len(primes) % 2 == 0:
+        prime_count = sum(primes[:n+1])
+        if prime_count % 2 == 0:
             ben_wins += 1
         else:
             maria_wins += 1
@@ -52,8 +32,7 @@ def isWinner(x, nums):
         return "Maria"
     elif ben_wins > maria_wins:
         return "Ben"
-    else:
-        return None
+    return None
 
 
 if __name__ == "__main__":
